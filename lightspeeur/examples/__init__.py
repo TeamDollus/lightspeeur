b8d4c127f1dbe609c168a55614532561e2e4afe6
OrigamiDream <sdy36071@naver.com>
2022-01-06 21:32:35 +0900

    Add package setuptools

---
+++ lightspeeur/examples/__init__.py
@@ -0,0 +1,2 @@
+from lightspeeur.examples import mnist
+from lightspeeur.examples import mobilenet
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='lightspeeur',
+      version='0.0.1',
+      description='Lightspeeur TensorFlow Mode Development Framework',
+      url='https://github.com/TeamDollus/lightspeeur.git',
+      author='OrigamiDream',
+      packages=find_packages(),
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=[
+            'tensorflow>=2.0',
+            'numpy',
+            'tqdm',
+      ])
