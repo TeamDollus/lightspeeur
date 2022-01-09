f28b8d74a4806f5f544b87fc7cca6cb12180da17
OrigamiDream <sdy36071@naver.com>
2022-01-10 04:05:48 +0900

    Update examples for model conversion

--- lightspeeur/examples/mnist.py
+++ lightspeeur/examples/mnist.py
@@ -1,8 +1,15 @@
+import random
+import time
+
 from tensorflow.keras import datasets, layers as l, Model
 from tensorflow.keras.models import load_model
 from lightspeeur.layers import Conv2D, MaxPooling2D, ReLU, DepthwiseConv2D
 from lightspeeur.models import ModelStageAdvisor, ModelConverter
-from lightspeeur.drivers import Driver
+from lightspeeur.drivers import Driver, Model as LightspeeurModel
+
+
+def current_milliseconds():
+    return round(time.time() * 1000)
 
 
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
@@ -60,17 +67,48 @@ else:
         'DepthwiseConv2D': DepthwiseConv2D,
         'ReLU': ReLU
     }
-    model = load_model('lightspeeur_mnist.h5')
+    model = load_model('lightspeeur_mnist.h5', custom_objects=custom_objects)
+
+# Conversion
+
+now = current_milliseconds()
+graph_name = 'conv'
 
 driver = Driver()
 converter = ModelConverter(chip_id=chip_id,
                            model=model,
                            graph={
-                               'conv': [['conv1/conv', 'conv1/batch_norm', 'conv1/relu'],
-                                        ['conv2/conv', 'conv2/relu'],
-                                        ['conv3/conv', 'conv3/batch_norm', 'conv3/relu']]
+                               graph_name: [['conv1/conv', 'conv1/relu'],
+                                            ['conv2/conv', 'conv2/relu'],
+                                            ['conv3/conv', 'conv3/relu']]
                            },
                            config_path='bin/libgticonfig2803.so',
                            debug=True)
 
-converter.convert(driver)
+results = converter.convert(driver)
+result = results[0]
+print('Time elapsed for conversion: {}ms'.format(current_milliseconds() - now))
+
+# Evaluate
+
+now = current_milliseconds()
+driver = Driver()
+lightspeeur_model = LightspeeurModel(driver, chip_id, result)
+samples = 5
+
+with lightspeeur_model:
+    # lightspeeur model uses __enter__() to load a libraries
+    print('Time elapsed for loading a model: {}ms'.format(current_milliseconds() - now))
+
+    for _ in range(samples):
+        now = current_milliseconds()
+        sample_index = random.randint(0, test_images.shape[0])  # 0 ~ 10000
+
+        sample_image = test_images[sample_index]
+        sample_label = test_labels[sample_index]
+
+        res = lightspeeur_model.evaluate(sample_image)
+        print()
+        print('Time elapsed for evaluate an image: {}ms'.format(current_milliseconds() - now))
+        print('Result shape: {}'.format(res.shape))
+
--- lightspeeur/examples/mobilenet.py
+++ lightspeeur/examples/mobilenet.py
@@ -1,12 +1,24 @@
+import time
+import random
+
 import tensorflow as tf
 
 from collections import namedtuple
 from tensorflow.keras import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense as KerasDense, Flatten, BatchNormalization
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense as KerasDense, Flatten, BatchNormalization, Input
 from lightspeeur.layers import DepthwiseConv2D, Conv2D, ReLU
-from lightspeeur.models import ModelStageAdvisor, LearningStage
+from lightspeeur.models import ModelStageAdvisor, ModelConverter
+from lightspeeur.drivers import Driver, Model as LightspeeurModel
 
+
+def current_milliseconds():
+    return round(time.time() * 1000)
+
+
+transfer_model = True
+train_model = True
+evaluate_model = False
 
 train_datagen = ImageDataGenerator(rescale=1 / 255.)
 valid_datagen = ImageDataGenerator(rescale=1 / 255.)
@@ -14,7 +26,7 @@ valid_datagen = ImageDataGenerator(rescale=1 / 255.)
 train_iter = train_datagen.flow_from_directory('dataset/train', target_size=(224, 224), batch_size=64)
 valid_iter = valid_datagen.flow_from_directory('dataset/valid', target_size=(224, 224), batch_size=64)
 
-chip_id = 2803
+chip_id = '2803'
 DepthwiseConvBlock = namedtuple("DepthwiseConvBlock", ["name", "stride", "in_channels", "out_channels"])
 MOBILENET_DEPTHWISE_CONV_LAYERS = [
     DepthwiseConvBlock(name="conv2_1", stride=1, in_channels=32, out_channels=64),
@@ -70,32 +82,93 @@ def build_mobilenet(inputs):
     return Model(name='MobileNetV1', inputs=inputs, outputs=outputs)
 
 
-custom_objects = {
-    'Conv2D': Conv2D,
-    'DepthwiseConv2D': DepthwiseConv2D,
-    'ReLU': ReLU
-}
-model = tf.keras.models.load_model('mobilenet_transferred.h5', custom_objects=custom_objects)
+# Transfer Learning
+if transfer_model:
+    custom_objects = {
+        'Conv2D': Conv2D,
+        'DepthwiseConv2D': DepthwiseConv2D,
+        'ReLU': ReLU
+    }
+    model = tf.keras.models.load_model('mobilenet_transferred.h5', custom_objects=custom_objects)
+else:
+    model = build_mobilenet(Input(shape=(224, 224, 3)))
 
-compile_options = {
-    'optimizer': 'adam',
-    'loss': 'categorical_crossentropy',
-    'metrics': ['accuracy']
-}
 
-advisor = ModelStageAdvisor(chip_id, model, compile_options)
-while True:
-    res = advisor.advance_stage()
-    if res:
-        if advisor.current_stage == LearningStage.MODEL_FUSION:
-            epochs = 200
+# Train the model
+if train_model:
+    compile_options = {
+        'optimizer': 'adam',
+        'loss': 'categorical_crossentropy',
+        'metrics': ['accuracy']
+    }
+
+    advisor = ModelStageAdvisor(chip_id, model, compile_options)
+    while True:
+        res = advisor.advance_stage()
+        if res:
+            advisor.propose(train_iter,
+                            epochs=10,
+                            validation_data=valid_iter,
+                            steps_per_epoch=train_iter.samples // train_iter.batch_size)
         else:
-            epochs = 10
-        advisor.propose(train_iter,
-                        epochs=epochs,
-                        validation_data=valid_iter,
-                        steps_per_epoch=train_iter.samples // train_iter.batch_size)
-    else:
-        break
+            break
 
-advisor.get_model().save('lightspeeur_mobilenet.h5')
+    advisor.get_model().save('lightspeeur_mobilenet.h5')
+    model = advisor.get_model()
+else:
+    custom_objects = {
+        'Conv2D': Conv2D,
+        'DepthwiseConv2D': DepthwiseConv2D,
+        'ReLU': ReLU
+    }
+    model = tf.keras.models.load_model('lightspeeur_mnist.h5', custom_objects=custom_objects)
+
+
+# Conversion
+now = current_milliseconds()
+graph_name = 'mobilenet'
+
+graph = [['conv1_1/conv', 'conv1_1']]
+for conv in MOBILENET_DEPTHWISE_CONV_LAYERS:
+    conv_name_depthwise = '{}_dw'.format(conv.name)
+    conv_name_pointwise = '{}_pw'.format(conv.name)
+    graph.append(['{}/conv'.format(conv_name_depthwise), conv_name_depthwise])
+    graph.append(['{}/conv'.format(conv_name_depthwise), conv_name_pointwise])
+
+driver = Driver()
+converter = ModelConverter(chip_id=chip_id,
+                           model=model,
+                           graph={
+                               graph_name: graph
+                           },
+                           config_path='bin/libgticonfig2803.so',
+                           debug=True)
+
+results = converter.convert(driver)
+result = results[0]
+print('Time elapsed for conversion: {}ms'.format(current_milliseconds() - now))
+
+
+# Evaluate
+if evaluate_model:
+    now = current_milliseconds()
+    driver = Driver()
+    lightspeeur_model = LightspeeurModel(driver, chip_id, result)
+    samples = 5
+
+    batch_images, batch_labels = valid_iter[0]
+
+    with lightspeeur_model:
+        print('Time elapsed for loading a model: {}ms'.format(current_milliseconds() - now))
+
+        for _ in range(samples):
+            now = current_milliseconds()
+            sample_index = random.randint(0, len(batch_images))
+
+            sample_image = batch_images[sample_index]
+            sample_label = batch_labels[sample_index]
+
+            res = lightspeeur_model.evaluate(sample_image)
+            print()
+            print('Time elapsed for evaluate an image: {}ms'.format(current_milliseconds() - now))
+            print('Result shape: {}'.format(res.shape))
