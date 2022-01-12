7d66eab17266928e0814c51ab686d9bef26c5f61
OrigamiDream <sdy36071@naver.com>
2022-01-13 02:56:52 +0900

    Update to 0.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='lightspeeur',
-      version='0.0.10',
+      version='0.0.11',
       description='Lightspeeur TensorFlow Mode Development Framework',
       url='https://github.com/TeamDollus/lightspeeur.git',
       author='OrigamiDream',
