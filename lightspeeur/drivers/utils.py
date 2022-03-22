d1791f0cafd9365e3a7b8874812ea44d1355e06f
OrigamiDream <sdy36071@naver.com>
2022-03-22 13:27:40 +0900

    Remove unnecessary utilities

--- lightspeeur/drivers/utils.py
+++ lightspeeur/drivers/utils.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 
-from lightspeeur.drivers.specification import Specification
-
 
 def permute_axis(axis_from, axis_to):
     return list(map(lambda x: axis_from.find(x), axis_to))
@@ -22,11 +20,5 @@ def constant_initializer(initializer, default_initializer):
     elif callable(initializer):
         return initializer
     else:
-        return tf.constant_initializer(initializer, verify_shape=True)
+        return tf.constant_initializer(initializer)
 
-
-def validate_image_fit_to_chip(buffer, size_axis=0):
-    shape = buffer[size_axis]
-    if shape not in Specification.ALLOWED_IMAGE_SIZES:
-        raise ValueError('Incompatible input image size: {}x{}'.format(shape, shape))
-    return buffer
